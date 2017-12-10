381e7066b71d61e4385de90d5f9f8d8ea646cf00
novalu <novalu@novalu.info>
2017-12-10 16:20:35 +0100

    Add cors to /data route.

--- routes/data.js
+++ routes/data.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const cors = require('cors');
+
 const storage = require('../storage/speech-storage');
 
-router.get('/:slug', function(req, res, next) {
+router.get('/:slug', cors(), function(req, res, next) {
   storage.getSpeechData(req.params.slug, function(err, data) {
     if (!err) {
       console.log(data);
