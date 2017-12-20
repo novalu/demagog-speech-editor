91f5080532d17679185331ea0f14c277e8904bb5
novalu <novalu@novalu.info>
2017-12-20 12:20:36 +0100

    Allow only defined slugs.

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,8 +8,21 @@ const auth = require("../utils/auth-utils");
 router.get('/', auth.middle, function(req, res, next) {
   provider.articlesList(function(err, data) {
     if (!err) {
-      // TODO filter only allowed articles
-      res.render('index', { articles: data.articles });
+      const allowedSlugs = [
+        "prezidentuv-projev-na-konferenci-hnuti-spd",
+        "prezidentske-interview-ct-michal-horacek",
+        "prezidentske-interview-ct-jiri-drahos",
+        "prezidentske-interview-ct-vratislav-kulhanek",
+        "prezidentske-interview-ct-jiri-hynek",
+        "prezidentske-interview-ct-petr-hannig",
+        "prezidentske-interview-ct-mirek-topolanek",
+        "prezidentske-interview-ct-marek-hilser",
+        "prezidentske-interview-ct-pavel-fischer"
+      ];
+      const filteredArticles = lodash.filter(data.articles, function(it) {
+        return lodash.indexOf(allowedSlugs, it.slug) !== -1
+      });
+      res.render('index', { articles: filteredArticles });
     } else {
       res.sendStatus(500)
     }
